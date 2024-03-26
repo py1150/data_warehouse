186497d600e24aa485290284f7bd934a84e49983
py1150 <berndgaller@gmail.com>
2024-03-26 11:07:10 +0100

    user and song tables create queries

--- sql_queries.py
+++ sql_queries.py
@@ -7,13 +7,13 @@ config.read('dwh.cfg')
 
 # DROP TABLES
 
-staging_events_table_drop = ""
-staging_songs_table_drop = ""
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+staging_events_table_drop = "DROP TABLE IF EXISTS staging_events;"
+staging_songs_table_drop = "DROP TABLE IF EXISTS staging_events;"
+songplay_table_drop = "DROP TABLE IF EXISTS songplay"
+user_table_drop = "DROP TABLE IF EXISTS user"
+song_table_drop = "DROP TABLE IF EXISTS song"
+artist_table_drop = "DROP TABLE IF EXISTS artist"
+time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
@@ -23,13 +23,40 @@ staging_events_table_create= ("""
 staging_songs_table_create = ("""
 """)
 
+   
+
 songplay_table_create = ("""
+    CREATE TABLE songplays (
+        songplay_id INTEGER NOT NULL,\
+        start_time TIMESTAMP NOT NULL,\
+        user_id  INTEGER NOT NULL,\
+        level VARCHAR(20) NOT NULL,\
+        song_id VARCHAR(50) NOT NULL,\
+        artist_id VARCHAR(50) NOT NULL,\
+        session_id INTEGER NOT NULL,\
+        location VARCHAR(100) NOT NULL,\
+        user_agent VARCHAR(100) NOT NULL\
+    );
 """)
 
 user_table_create = ("""
+    CREATE TABLE users (
+        user_id INTEGER NOT NULL,\
+        first_name VARCHAR(50) NOT NULL,\
+        last_name VARCHAR(50) NOT NULL,\
+        gender VARCHAR(5) NOT NULL,\                     
+        level VARCHAR(20) NOT NULL
+    );
 """)
 
 song_table_create = ("""
+    CREATE TABLE songs (
+        song_id VARCHAR(50) NOT NULL,\
+        title VARCHAR(200) NOT NULL,\
+        artist_id VARCHAR(50) NOT NULL,\
+        year INTEGER NOT NULL,\            
+        duration DOUBLE PRECISION NOT NULL
+    );
 """)
 
 artist_table_create = ("""
@@ -39,6 +66,16 @@ time_table_create = ("""
 """)
 
 # STAGING TABLES
+
+json mapping must be inserted
+%%time
+qry = """
+    copy sporting_event_ticket from 's3://udacity-labs/tickets/split/part'
+    credentials 'aws_iam_role={}'
+    gzip delimiter ';' compupdate off region 'us-west-2';
+""".format(DWH_ROLE_ARN)
+
+%sql $qry
 
 staging_events_copy = ("""
 """).format()
